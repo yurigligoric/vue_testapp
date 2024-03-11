e613a41d82e63d0c950e231893867c2c46a9dfd5
yurigligoric <yurigligoric@gmail.com>
2024-03-11 10:22:30 +0800

    firts commit test vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Vue.js App"/>
+  <HiWorld />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HiWorld from './components/HiWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, 
+    HiWorld
   }
 }
 </script>
---
+++ src/components/HiWorld.vue
@@ -0,0 +1,19 @@
+<!-- HiWorld.vue -->
+
+<template>
+  <div>
+    <h2>Hi World Component</h2>
+    <p>This is the HiWorld component content.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HiWorld',
+  // You can add component-specific logic here
+}
+</script>
+
+<style scoped>
+/* Add component-specific styles here */
+</style>
